6d4f5b2ed783ce1bd2ce078ee5e8f04226f06e85
Dex Fitch and Mike Goren <fitch.dexter@gmail.com>
2015-02-12 10:49:38 -0800

    Add new dimension of maintaining order of appearance of word in returned results, refactor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,49 @@
 var orderify = function(words) {
   var orderedWords = [];
-
-  var splitWords = words.split(" ");
-  splitWords.sort();
-
   var wordsWithCount = [];
-  var previousWord = "";
+  var previousWords = [];
+  var splitWords = words.split(" ");
+  var indexOfWord = 0;
+
   splitWords.forEach(function(word) {
-    if (word !== previousWord) { // if new word
-      previousWord = word;
+    if ( previousWords.indexOf(word) === -1 ) { // if new word
+      previousWords.push(word);
       wordsWithCount.push([1, word]);
     } else { // if is another occurence of previous word
-      var indexOfLastElement = wordsWithCount.length - 1;
-      wordsWithCount[indexOfLastElement][0]++;
+      wordsWithCount.forEach(function(wordAndCount, index) {
+        if (wordAndCount[1] === word) {
+          indexOfWord = index;
+        }
+      });
+      wordsWithCount[indexOfWord][0]++;
     }
   });
 
   // each element in wordsWithCount IS an array [count, word]
 
   // array now looks like this:
-  // [ [1, apple] [2, candy] [1, donut] ]
+  // [ [1, "apple"], [2, "candy"], [1, "abnut"] ]
 
-  wordsWithCount.sort().reverse();
+  wordsWithCount.sort(function(a, b) {
+    if (a[0] < b[0]) {
+      return 1;
+    }
+    if (a[0] > b[0]) {
+      return -1;
+    }
+    // a[0] === b[0]
+    return 0;
+  });
 
   // array now looks like this:
-  // [ [2, candy] [1, donut] [1, apple] ]
+  // [ [2, "candy"], [1, "apple"], [1, "abnut"] ]
 
   wordsWithCount.forEach(function(wordAndCount) {
     orderedWords.push(wordAndCount[1]);
   });
 
   // array now looks like this:
-  // ["candy", "donut", "apple"]
+  // ["candy", "apple", "abnut"]
 
   return orderedWords;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -4,17 +4,12 @@ describe('orderify', function() {
     expect(orderify("coffee")).to.eql(["coffee"]);
   });
 
-  it("returns a phrase in an array", function() {
-    expect(orderify("coffee now plz")).to.eql(["plz", "now", "coffee"]);
+  it("returns a phrase in an array in original order", function() {
+    expect(orderify("coffee now plz")).to.eql(["coffee", "now", "plz"]);
   });
 
-  it("returns an array in reverse sorted order", function() {
-    expect(orderify("boats full of candy apples")).to.eql(["of", "full", "candy", "boats", "apples"]);
+  it("returns an array with words arranged by order of frequency and appearance", function() {
+    expect(orderify("boats full of candy apples candy")).to.eql(["candy", "boats", "full", "of", "apples"]);
   });
-
-  it("returns an array with words arranged by order of frequency", function() {
-    expect(orderify("candy boats full of candy apples")).to.eql(["candy", "of", "full", "boats", "apples"]);
-  });
-
 
 });
